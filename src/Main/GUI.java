baade39ef27cbd5ef0285339765122ebe11adf60
radames <radames@x50>
2019-07-04 15:50:09 -0300

    melhorzinho

--- src/Main/GUI.java
+++ src/Main/GUI.java
@@ -27,6 +27,10 @@ public class GUI extends JFrame {
     JButton btAdicionar = new JButton("Adicionar");
     JButton btListar = new JButton("Listar");
     JButton btBuscar = new JButton("Buscar");
+    JButton btAlterar = new JButton("Alterar");
+    JButton btExcluir = new JButton("Excluir");
+    JButton btSalvar = new JButton("Salvar");
+    JButton btCancelar = new JButton("Cancelar");
 
     Controle controle = new Controle();
     Trabalhador trabalhador = new Trabalhador();
@@ -40,7 +44,7 @@ public class GUI extends JFrame {
         setLocationRelativeTo(null);//centro do monitor
 
         cp = getContentPane();
-        cp.setLayout(new GridLayout(5, 2));
+        cp.setLayout(new GridLayout(7, 2));
         cp.add(lbCpf);
         cp.add(tfCpf);
         cp.add(lbNome);
@@ -50,24 +54,36 @@ public class GUI extends JFrame {
         cp.add(btAdicionar);
         cp.add(btListar);
         cp.add(btBuscar);
+        cp.add(btAlterar);
+        cp.add(btExcluir);
+        cp.add(btSalvar);
+        cp.add(btCancelar);
 
         btAdicionar.setVisible(false);
+        btAlterar.setVisible(false);
+        btExcluir.setVisible(false);
+        btSalvar.setVisible(false);
+        btCancelar.setVisible(false);
 
         btBuscar.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 if (tfCpf.getText().trim().isEmpty()) {
-                    JOptionPane.showMessageDialog(cp, "CPF nâo pode ser vazio");
+                    JOptionPane.showMessageDialog(cp,
+                            "CPF nâo pode ser vazio");
                     tfCpf.requestFocus();
                     tfCpf.selectAll();
                 } else {
-                    Trabalhador trabalhador
-                            = controle.buscar(tfCpf.getText());
-                    if (trabalhador == null) {
+                    trabalhador = controle.buscar(tfCpf.getText());
+                    if (trabalhador == null) {//nao encontrou
                         btAdicionar.setVisible(true);
-                    } else {
+                        btAlterar.setVisible(false);
+                        btExcluir.setVisible(false);
+                    } else {//encontrou
                         tfNome.setText(trabalhador.getNome());
                         tfSalario.setText(String.valueOf(trabalhador.getSalario()));
+                        btAlterar.setVisible(true);
+                        btExcluir.setVisible(true);
                     }
                 }
             }
@@ -82,6 +98,75 @@ public class GUI extends JFrame {
                 trabalhador.setSalario(Double.valueOf(tfSalario.getText()));
                 controle.adicionar(trabalhador);
                 btAdicionar.setVisible(false);
+            }
+        });
+
+        btAlterar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                tfCpf.setEditable(false);
+                tfNome.requestFocus();
+                btSalvar.setVisible(true);
+                btCancelar.setVisible(true);
+                btBuscar.setVisible(false);
+                btListar.setVisible(false);
+                btAlterar.setVisible(false);
+                btExcluir.setVisible(false);
+            }
+        });
+
+        btCancelar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                btSalvar.setVisible(false);
+                btCancelar.setVisible(false);
+                btBuscar.setVisible(true);
+                btListar.setVisible(true);
+                tfCpf.setEditable(true);
+                tfNome.setText("");
+                tfSalario.setText("");
+                tfCpf.requestFocus();
+                tfCpf.selectAll();
+            }
+        });
+
+        btSalvar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Trabalhador trabalhadorAntigo = trabalhador;
+                trabalhador.setNome(tfNome.getText());
+                trabalhador.setSalario(Double.valueOf(tfSalario.getText()));
+                controle.alterar(trabalhador, trabalhadorAntigo);
+                btSalvar.setVisible(false);
+                btCancelar.setVisible(false);
+                btBuscar.setVisible(true);
+                btListar.setVisible(true);
+                tfCpf.setEditable(true);
+                tfNome.setText("");
+                tfSalario.setText("");
+                tfCpf.requestFocus();
+                tfCpf.selectAll();
+            }
+        });
+        btExcluir.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (JOptionPane.YES_OPTION
+                        == JOptionPane.showConfirmDialog(null,
+                                "Confirma a exclusão do registro <Nome = " + trabalhador.getNome() + ">?", "Confirm",
+                                JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE)) {
+                    controle.excluir(trabalhador);
+                }
+                btBuscar.setVisible(true);
+                btListar.setVisible(true);
+                tfCpf.setEditable(true);
+                tfNome.setText("");
+                tfSalario.setText("");
+                tfCpf.requestFocus();
+                tfCpf.selectAll();
+                btExcluir.setVisible(false);
+                btAlterar.setVisible(false);
+
             }
         });
 
