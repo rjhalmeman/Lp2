39fc187b21f87ea005a875e9ac094bbed3e64a42
Radames J Halmeman <rjhalmeman@gmail.com>
2019-07-05 06:49:39 -0300

    Crud Canguru Base para Avaliação do 2° Bimestre

--- src/Main/Controle.java
+++ src/Main/Controle.java
@@ -1,4 +1,3 @@
-
 package Main;
 
 import java.util.ArrayList;
@@ -9,52 +8,49 @@ import java.util.List;
  * @author radames
  */
 public class Controle {
-    
+
     List<Trabalhador> lista = new ArrayList<>();
 
-    public Controle() {
+    public Controle() { //esse construtor é usado para adicionar alguns dados na lista e 
+        //facilitar os testes, quando fizermos a persistência em arquivo ele deverá ser excluído
         Trabalhador t = new Trabalhador();
         t.setCpf("123");
-        t.setNome("jao");
+        t.setNome("Timocréia");
         t.setSalario(5000);
         adicionar(t);
-        
-        
         t = new Trabalhador("456", "Berola", 3252.97);
         adicionar(t);
-        
-        
-        t = new Trabalhador("789", "Ana", 0.30);
+        t = new Trabalhador("789", "Reduzina", 3000.30);
+        adicionar(t);
+        t = new Trabalhador("111", "Zulida", 3000.30);
         adicionar(t);
     }
-    
-    
-    public void adicionar(Trabalhador trabalhador){
+
+    public void adicionar(Trabalhador trabalhador) {
         lista.add(trabalhador);
     }
-    
-    public List<Trabalhador> listar(){
+
+    public List<Trabalhador> listar() {
         return lista;
     }
-    
-   public Trabalhador buscar(String cpf){
-       for (int i = 0; i < lista.size(); i++) {
-           if (lista.get(i).getCpf().equals(cpf)) {
-               return lista.get(i);
-           }
-       }
-       return null;
-      
-   }
+
+    public Trabalhador buscar(String cpf) {
+        for (int i = 0; i < lista.size(); i++) {
+            if (lista.get(i).getCpf().equals(cpf)) {
+                return lista.get(i);
+            }
+        }
+        return null;
+
+    }
 
     public void alterar(Trabalhador trabalhador, Trabalhador trabalhadorAntigo) {
-       lista.set(lista.indexOf(trabalhadorAntigo), trabalhador);
-      
-       
+        lista.set(lista.indexOf(trabalhadorAntigo), trabalhador);
+
     }
 
     public void excluir(Trabalhador trabalhador) {
-       lista.remove(trabalhador);
+        lista.remove(trabalhador);
     }
-    
+
 }
