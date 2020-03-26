4a9883b37eee5bce09bf8e4f787e27177291402d
radames <radames@midnight>
2020-03-26 08:54:27 -0300

    Interface Gráfica. BorderLayout.

--- src/Main/GUI.java
+++ src/Main/GUI.java
@@ -1,9 +1,12 @@
 
 package Main;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Container;
-import java.awt.GridLayout;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 /**
  *
@@ -11,20 +14,36 @@ import javax.swing.JFrame;
  */
 class GUI extends JFrame{
     Container cp;
+    JPanel pnNorte = new JPanel();
+    JPanel pnSul = new JPanel();
+    JPanel pnOeste = new JPanel();
+    JPanel pnLeste = new JPanel();
+    JPanel pnCentro = new JPanel();
+    JLabel lbNorte = new JLabel("label no painel norte");
+    
     public GUI() {
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);
-        setTitle("Título da janela");
+        setTitle("Exemplo de Border Layout");
         
         cp = getContentPane();
-        cp.setLayout(new GridLayout(3,2));
+        cp.setLayout(new BorderLayout());
         
+        cp.add(pnNorte,BorderLayout.NORTH);
+        cp.add(pnSul,BorderLayout.SOUTH);
+        cp.add(pnOeste,BorderLayout.WEST);
+        cp.add(pnLeste,BorderLayout.EAST);
+        cp.add(pnCentro, BorderLayout.CENTER);
+        
+        pnNorte.setBackground(Color.cyan);
+        pnSul.setBackground(Color.green);
+        pnOeste.setBackground(Color.PINK);
+        pnLeste.setBackground(Color.YELLOW);
+        pnCentro.setBackground(Color.white);
+        
+        pnNorte.add(lbNorte);
         
         setSize(400, 300);
         setLocationRelativeTo(null);
-        setVisible(true);
-        
-    }
-    
-    
-    
+        setVisible(true);        
+    }  
 }
